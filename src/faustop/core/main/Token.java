cc1f4d4fd35e5775c822efde436e8633de3a3fcf
Junior-16 <junior.ramisch@gmail.com>
2019-04-28 15:25:15 -0300

    testing jean's code

--- src/faustop/core/main/Token.java
+++ src/faustop/core/main/Token.java
@@ -1,43 +1,43 @@
 package faustop.core.main;
 
 class Token {
-    
+
     private String type;
     private String name;
     private int row;
     private int col;
-    
+
     public Token(String t, String n, int r, int c) {
         this.setType(t);
         this.setName(n);
         this.setRow(r);
         this.setCol(c);
     }
-    
+
     public String getType() {
-        return this.type;    
+        return this.type;
     }
-    
+
     public void setType(String pType) {
         this.type = pType;
     }
-    
+
     public String getName() {
-        return this.name;    
+        return this.name;
     }
-    
+
     public void setName(String pName) {
         this.name = pName;
     }
-    
+
     public int getRow() {
         return this.row;
     }
-    
+
     public void setRow(int pRow) {
         this.row = pRow;
     }
-    
+
     public int getCol() {
         return this.col;
     }
@@ -45,5 +45,4 @@ class Token {
     public void setCol(int pCol) {
         this.col = pCol;
     }
-    
 }
--- src/faustop/core/main/tester.java
+++ src/faustop/core/main/tester.java
@@ -1,10 +1,10 @@
 package faustop.core.main;
 
 class Main {
-    
+
     public static void main(String[] args) {
         Token a = new Token("ant", "ID", 1, 3);
-        
+
         System.out.println(a.getName() + a.getType() + a.getRow() + a.getCol());
     }
     
