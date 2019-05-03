c427ada39eb1d10854b816179b151415e3a12609
Junior-16 <junior.ramisch@gmail.com>
2019-05-03 03:42:39 -0300

    Wrapper class created to integrate lexer and parser tests

--- src/faustop/core/main/tester.java
+++ src/faustop/core/main/tester.java
@@ -6,18 +6,20 @@ class Nain {
 		/*String a = "voce destr\n? \n 1u+=iu \n me > =9;()u ovo?Pais da Europa\n";
 		String b = "como voce\n\n 1+= 1?\n é gostosa\n";
 		String c = "if\n(a<4)       {\nmostrai(\"MEU OVO\")\n}\n";
-		Lexer lutor = new Lexer();
 		// lutor.setCode(a);
-		lutor.setCode(a);
 		// lutor.setCode(c);
+		//*/
+		
+		String command = "int @ = 4;\n";
+		Lexer lutor = new Lexer();
+		lutor.setCode(command);
 		Token cu = lutor.getNextToken();
-		//
 		while (cu != null) {
 			System.out.println(cu.getName() + " " + cu.getType() + " - " + cu.getRow() + ":" + cu.getCol());
 			cu = lutor.getNextToken();
-		}*/
+		}
 
-		Tree t = new Tree();
+		/*Tree t = new Tree();
 		Token to;
 
 		for (int i =0; i < 50; i++) {
@@ -26,7 +28,7 @@ class Nain {
 			t.addNode(to);
 		}
 
-		t.traverse(t.root);
+		t.traverse(t.root);*/
 
 	}
 
