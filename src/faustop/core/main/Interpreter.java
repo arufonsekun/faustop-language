0d94448ca08cd4c27195614d01aef7c33ea4827f
aluno-uffs <aluno-uffs@github.com>
2019-05-07 11:49:35 -0300

    Interpreter and Wrapper classes updated.

---
+++ src/faustop/core/main/Interpreter.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import vars.Variable;
+
+class Interpreter {
+
+    private HashMap<String, Variable> ram = new HashMap<String, Variable>();
+    // private Tree parseTree;
+
+    public void run(Node pTreeRoot) {
+
+        if (pTreeRoot == null) return;
+
+        for (Node child : pTreeRoot.children()) {
+            this.run(child);
+        }
+        System.out.println(pTreeRoot.key().getName());
+
+    }
+
+}
