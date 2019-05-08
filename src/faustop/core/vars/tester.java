7202e8cfe31043b97eb44118361ce3a8baf77726
Junior-16 <junior.ramisch@gmail.com>
2019-05-07 23:14:01 -0300

    resolving conflicts

--- src/faustop/core/vars/tester.java
+++ src/faustop/core/vars/tester.java
@@ -4,19 +4,9 @@ class Main {
         Integer_ b = new Integer_("b", "1");
         Integer_ c = new Integer_("c", "3");
         Double_ d = new Double_("d", "1.1");
-<<<<<<< HEAD
-        Double_ e = new Double_("e", "1.1");
-        String_ f = new String_("f", "miau");
-        String_ g = new String_("g", "miau");
-
-        // a.equals(b);
-        // d.times(b);
-        // System.out.println(f.greaterThanOrEqualTo(g));
-=======
         Double_ e = new Double_("e", "1.2");
 
         a.equals(b);
->>>>>>> 8b1165647ead12ecf8af6a87e1a6995260fed1d4
 
         // System.out.println(b.greaterThan(d));
         // System.out.println(d.getValue());
