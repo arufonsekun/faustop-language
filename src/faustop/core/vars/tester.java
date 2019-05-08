b47a5f1591119c5809d7809bbd64c771e2776341
Junior-16 <junior.ramisch@gmail.com>
2019-05-07 23:10:19 -0300

    deleting files

---
+++ src/faustop/core/vars/tester.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {
+        Boolean_ a = new Boolean_("a", "true");
+        Integer_ b = new Integer_("b", "1");
+        Integer_ c = new Integer_("c", "3");
+        Double_ d = new Double_("d", "1.1");
+        Double_ e = new Double_("e", "1.1");
+        String_ f = new String_("f", "miau");
+        String_ g = new String_("g", "miau");
+
+        // a.equals(b);
+        // d.times(b);
+        // System.out.println(f.greaterThanOrEqualTo(g));
+
+        // System.out.println(b.greaterThan(d));
+        // System.out.println(d.getValue());
+    }
+}
